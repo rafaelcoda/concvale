f43593f8f56b463a320506fd08edf79b30677bcf
Rafael Coda <rafacoda@gmail.com>
2019-05-07 17:27:35 -0300

    first commit

---
+++ src/main/java/com/example/concvale/ConcvaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.concvale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcvaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcvaleApplication.class, args);
+	}
+
+}
